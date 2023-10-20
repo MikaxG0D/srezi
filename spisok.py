b1a4bc1aec78d965a3c58c8e0dddf550e85f3a0c
MikaxG0D <143862234+MikaxG0D@users.noreply.github.com>
2023-10-20 22:03:22 +0700

    Add files via upload

---
+++ spisok.py
@@ -0,0 +1,20 @@
+a = input()
+
+#№1
+print(a[2])
+#№2
+print(a[-2])
+#№3
+print(a[:5])
+#№4
+print(a[:-2])
+#№5
+print(a[::2])
+#№6
+print(a[1::2])
+#№7
+print(a[::-1])
+#№8
+print(a[::-2])
+#№9
+print(len(a))
